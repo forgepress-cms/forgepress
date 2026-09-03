1850c4e413afcbe7e0b493d1342a3855e3b76585
Frederik Bußmann <58994540+freb97@users.noreply.github.com>
2026-09-03 15:50:21 +0200

    content: test

--- playgrounds/nuxt/.webenv/content/author.ts
+++ playgrounds/nuxt/.webenv/content/author.ts
@@ -28,11 +28,11 @@ export default defineWebenvContent<'author'>([
       height: 1080,
     },
     biography: {
-      en: 'John Smith is a product manager with a background in design and user experience. He has worked on a variety of products, from mobile apps to enterprise software, and is passionate about creating intuitive and user-friendly experiences.',
+      en: 'John Smith is a product manager with a background in design and user experience. He has worked on a variety of products, from mobile apps to enterprise software, and is passionate about creating intuitive and user-friendly experiences. Test.',
       de: 'John Smith ist Product Manager mit Hintergrund in Design und User Experience. Er hat an verschiedenen Produkten gearbeitet, von Mobile-Apps bis Unternehmenssoftware, und begeistert sich fur intuitive, benutzerfreundliche Erlebnisse.',
     },
     blogPosts: ['blogPost_b65b9cf77fca'],
     createdAt: '2024-02-01T00:00:00Z',
-    updatedAt: '2026-09-01T17:16:21.814Z',
+    updatedAt: '2026-09-03T08:44:24.143Z',
   },
 ])
